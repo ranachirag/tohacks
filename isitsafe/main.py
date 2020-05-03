049b5688f451f31238eee7ea5f8375734fc7e28b
Chirag Rana <chirag.rana@mail.utoronto.ca>
2020-05-03 11:38:17 -0400

    Improved commenting

--- isitsafe/main.py
+++ isitsafe/main.py
@@ -2,10 +2,6 @@ import populartimes, requests
 from flask import Flask, request, render_template
 
 api_key = "AIzaSyA598cf-Rj1uh07ZZwLxFwkipQQJj8NUAE"
-# user_input = "bar"
-# lat = (43.750126, -79.639509,)
-# long = (43.723822, -79.055455,)
-# id = populartimes.get(api, [user_input], lat, long)
 
 app = Flask(__name__)
 
@@ -19,31 +15,19 @@ def homepage2():
     # url variable store url
     url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
 
-    # The text string on which to search
-    # query = input('Search query: ')
-
-    # get method of requests module
-    # return response object
     r = requests.get(url + 'query=' + query +
                      '&key=' + api_key)
 
-    # json method of response object convert
-    #  json format data into python format data
     x = r.json()
 
-    # now x contains list of nested dictionaries
-    # we know dictionary contain key value pair
-    # store the value of result key in variable y
     y = x['results']
     data = {}
     data['name'] = []
     data['address'] = []
     data['current_popularity'] = []
     data['rating'] = []
-    # keep looping upto length of y
+
     for i in range(len(y)):
-
-
         f = populartimes.get_id(api_key, y[i]['reference'])
         if "name" in f.keys():
             data['name'].append(f['name'])
